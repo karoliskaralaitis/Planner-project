9f5f5d7ca3b3bc4a22fcea8824807054b69ce4ce
Karolis <karalaitisk@gmail.com>
2024-01-22 17:50:59 +0200

    1. [Spring MVC] updated pom.xml with Thymeleaf dependency.

--- src/main/java/com/karalaitis/planner/FirstController.java
+++ src/main/java/com/karalaitis/planner/FirstController.java
@@ -2,12 +2,15 @@ package com.karalaitis.planner;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FirstController {
 
-    @GetMapping("/hello")
-    public String sayHello(){
-        return "hello.html";
+    @GetMapping("/hello/{name}")
+    public String sayHello(@PathVariable String name, @RequestParam String surname){
+        System.out.println("Hello, my name is " + name + " " + surname);
+        return "hello";
     }
 }
