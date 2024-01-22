f4b133b484c23a51d8e894bed58ff40b53fbce9b
Karolis <karalaitisk@gmail.com>
2024-01-22 17:28:14 +0200

    1. [Spring MVC] initial commit with test controller.

---
+++ src/main/java/com/karalaitis/planner/FirstController.java
@@ -0,0 +1,13 @@
+package com.karalaitis.planner;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "hello.html";
+    }
+}
