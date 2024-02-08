07c45f2f7852aac1fc9a0d4333de20b3fb43baa7
Karolis <karalaitisk@gmail.com>
2024-02-09 00:24:13 +0200

    5. [Spring MVC, DATA JPA] Added HTTP endpoints class.

---
+++ src/main/java/com/karalaitis/planner/HttpEndpoints.java
@@ -0,0 +1,9 @@
+package com.karalaitis.planner;
+
+public class HttpEndpoints {
+
+    public static final String GOALS = "/goals";
+    public static final String GOALS_CREATE = GOALS + "/create";
+    public static final String GOALS_UPDATE = GOALS + "/{goalId}/update";
+    public static final String GOALS_DELETE = GOALS + "/{goalId}/delete";
+}
--- src/main/java/com/karalaitis/planner/goals/controllers/GoalController.java
+++ src/main/java/com/karalaitis/planner/goals/controllers/GoalController.java
@@ -1,5 +1,6 @@
 package com.karalaitis.planner.goals.controllers;
 
+import com.karalaitis.planner.HttpEndpoints;
 import com.karalaitis.planner.goals.Goal;
 import com.karalaitis.planner.goals.dto.GoalDto;
 import com.karalaitis.planner.goals.service.GoalService;
@@ -26,39 +27,39 @@ public class GoalController {
         this.goalService = goalService;
     }
 
-    @GetMapping("/goals/create")
+    @GetMapping(HttpEndpoints.GOALS_CREATE)
     public String showGoalCreationPage(Model model){
         model.addAttribute("goal", Goal.builder().build());
         return "goals/goalsCreate";
     }
 
-    @GetMapping("/goals/{goalId}/update")
+    @GetMapping(HttpEndpoints.GOALS_UPDATE)
     public String showGoalUpdatePage(Model model, @PathVariable UUID goalId){
         model.addAttribute("goal", goalService.getGoalByUUID(goalId));
         return "goals/goalsCreate";
     }
 
-    @PostMapping("/goals/create")
+    @PostMapping(HttpEndpoints.GOALS_CREATE)
     public String createGoal(Model model, Goal goal) {
         goalService.saveGoal(goal);
         model.addAttribute("message", "Goal added successfully!");
-        return "goals/goalsCreate";
+        return "goals/create";
     }
 
-    @PostMapping("/goals/{goalId}/update")
+    @PostMapping(HttpEndpoints.GOALS_UPDATE)
     public String updateGoal(Model model, Pageable pageable, Goal goal, @PathVariable UUID goalId) {
         goalService.updateGoal(goal);
         return getGoals(model, pageable);
     }
 
-    @GetMapping("/goals")
+    @GetMapping(HttpEndpoints.GOALS)
     public String getGoals(Model model, @PageableDefault(size = 3, sort = {"doByDate"}, direction = Sort.Direction.ASC) Pageable pageable){
         final Page<GoalDto> allGoals = goalService.getAllGoalsPage(pageable);
         model.addAttribute("goalList", allGoals);
         return "goals/goalsList";
     }
 
-    @GetMapping("/goals/{goalId}/delete")
+    @GetMapping(HttpEndpoints.GOALS_DELETE)
     public String deleteGoal(Model model, Pageable pageable, @PathVariable UUID goalId) {
         goalService.deleteGoalByUUID(goalId);
         return getGoals(model, pageable);
