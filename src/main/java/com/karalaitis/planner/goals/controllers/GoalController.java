81bc53e28fef3ba424b2938aaa28c769ef114d90
Karolis <karalaitisk@gmail.com>
2024-02-08 15:56:18 +0200

    4. [Spring JPA] Added PageableDefault annotation instead of application.yml parameter; Added pagination bar stub.

--- src/main/java/com/karalaitis/planner/goals/controllers/GoalController.java
+++ src/main/java/com/karalaitis/planner/goals/controllers/GoalController.java
@@ -3,21 +3,19 @@ package com.karalaitis.planner.goals.controllers;
 import com.karalaitis.planner.goals.Goal;
 import com.karalaitis.planner.goals.dto.GoalDto;
 import com.karalaitis.planner.goals.service.GoalService;
-import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.ui.Model;
 
-import java.util.List;
 import java.util.UUID;
 
 @Controller
-@Log4j2
 public class GoalController {
 
     private GoalService goalService;
@@ -53,7 +51,7 @@ public class GoalController {
     }
 
     @GetMapping("/goals")
-    public String getGoals(Model model, Pageable pageable){
+    public String getGoals(Model model, @PageableDefault(size = 3) Pageable pageable){
         final Page<GoalDto> allGoals = goalService.getAllGoalsPage(pageable);
         model.addAttribute("goalList", allGoals);
         return "goals/goalsList";
